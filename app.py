d2f3a36371d3d928b7ba6c3a693671342d165d97
cj7mo <cj7mo@outlook.com>
2024-10-12 21:19:01 -0500

    app updated

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ logo = Image.open('img/logo.png')
 col1, col2 = st.columns([0.1, 0.9])
 
 url = 'https://raw.githubusercontent.com/CJ7MO/price-predictor-system-ns/refs/heads/main/data/df_model.csv'
-df = pd.read_csv(url, index_col=0)
+df = pd.read_csv(url)
 
 reg = xgb.XGBRegressor(objective='reg:squarederror', random_state=42, learning_rate=0.35)
 X = df.drop(columns=['Precio'])
